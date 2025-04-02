b0fcef542d8cfaf73828caa40c339a2808c1842a
Daniel Ehrhardt <daniel.ehrhardt@codext.de>
2025-04-02 20:25:14 +0200

    Added Android

--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,32 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { ZendeskPlugin } from './definitions';
+import type { ZendeskPlugin, AnonymousOptions, HelpCenterOptions, IdentityOption, InitializeOptions, TicketRequestOptions } from './definitions';
+
 
 export class ZendeskWeb extends WebPlugin implements ZendeskPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  
+  async initialize(options: InitializeOptions): Promise<void> {
+    console.log('initialize not implemented on web yet!', options);
+  }
+
+  async setAnonymousIdentity(options: AnonymousOptions): Promise<void> {
+    console.log('setAnonymousIdentity not implemented on web yet!', options);
+  }
+
+  async setIdentity(option: IdentityOption): Promise<void> {
+    console.log('setIdentity not implemented on web yet!', option);
+  }
+
+  async showHelpCenter(options: HelpCenterOptions): Promise<void> {
+    console.log('showHelpCenter not implemented on web yet!', options);
+  }
+
+  async showTicketRequest(options: TicketRequestOptions): Promise<void> {
+    console.log('showTicketRequest not implemented on web yet!', options);
+  }
+
+  async showUserTickets(): Promise<void> {
+    console.log('showUserTickets not implemented on web yet!');
   }
 }
+
